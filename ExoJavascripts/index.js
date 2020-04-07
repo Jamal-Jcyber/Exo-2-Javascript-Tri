f10e88fd6b72acc13454bbb27f73de7191e5a18f
Jamal-Jcyber <amimijamal@outlook.fr>
2020-04-07 19:55:34 +0100

    All Good

--- ExoJavascripts/index.js
+++ ExoJavascripts/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
-const User = require('./src/Model/User');
+
 
 const app = express();
 
@@ -27,29 +27,67 @@ connection.connect((err) => {
 });
 
 
-// @ routes
-// @ desc
+
+// @ route get the home page.
+// @ desc:
+
 app.get('/', (req, res, next) => {
     //res.send('It\'s Work Correctly');
     res.render('index.ejs', { title: 'Formula For Adding New Record'})
 });
 
-// @ POST Method to add new record to the database
-app.post('/', (req, res, next) => {
+
+
+// @ route get all data from the database.
+// desc:
+
+app.get('/all', (req, res, next) => {
+    const sql = "SELECT * FROM users";
+    connection.query(sql, (err, data) => {
+        if(err) throw err
+        res.render('display.ejs', {title: 'Here You Have The All Records', records:data });
+    });
+});
+
+// @ route classification using age
+// desc:
+
+app.get('/all/age', (req, res, next) => {
+    const sql = 'SELECT * FROM users ORDER BY Age DESC';
+    connection.query(sql, (err, data) => {
+        if(err) throw err
+        res.render('age.ejs', { title: 'This Table Classified With The Age', records:data});
+    });
+});
+
+// @ route classification using Alphabet
+// desc:
+
+app.get('/all/alpha', (req, res, next) => {
+    const sql = 'SELECT * FROM users ORDER BY FirstName ASC';
+    connection.query(sql, (err, data) => {
+        if(err) throw err
+        res.render('alpha.ejs', { title: 'This Table Classified With The FirstName', records:data});
+    });
+});
+
+// @ POST Method to add new record to the database.
+// desc:
+
+app.post('/add', (req, res, next) => {
 
     const input = req.body;
-    const user = new User({
-        fname : input.firstname,
-        lname : input.lastname,
-        age   :   input.age,
-        prof  :  input.profession
-    });
 
-    const sql = 'INSERT INTO `users` (`firstname`, `lastname`, `age`, `profession`) VALUES ('+user.fname+', '+user.lname+', '+user.age+', '+user.prof+')';
+        fname = input.firstname;
+        lname = input.lastname;
+        age   =   input.age;
+        prof  =  input.profession;
     
+    const sql = " INSERT INTO users (FirstName, LastName, Age, Profession) VALUES ('"+fname+"', '"+lname+"', '"+age+"', '"+prof+"')";
+
     connection.query(sql, (err, result) => {
         if(err) throw err
-        res.render('index.ejs', { message_success: 'Record was Added Successfully to the DataBase', data:result});
+        res.render('index.ejs', { message_success: 'Record was Added Successfully to the DataBase'});
     });
 
 });
